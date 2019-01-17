39b7d6918a03947203fec852d3a9b2166babbf43
Zac Seewald <zaxblackdragon.com>
2019-01-17 17:38:48 -0600

    added pages folder to reconcile

---
+++ client/src/pages/login.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+class LogIn extends Component {
+  render() {
+    return (
+     <div className="Site">
+      <div className="Site-content">
+
+        {/* Header */}
+        <div classname="App-header">
+          <Header />
+        </div>
+
+        {/* Main Game Card content goes below: */}
+        <div className="main">
+        
+        </div>
+          
+        {/* Footer */}
+        <Footer />
+        </div>
+
+     </div>
+    )
+  }
+}
+
+
+
+export default LogIn;
+
---
+++ client/src/pages/test.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Testing testing 123</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
