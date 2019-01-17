20654ee096d6b279ec3845fd3e4ecb0594e832c5
Zac Seewald <zaxblackdragon.com>
2019-01-17 17:33:09 -0600

    updates to Header.js for reconcile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import logo from '../images/header/zooapocolypsLogo.png';
 
 export class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Some cool stuff</h1>
-        <p style={pStyle} >and App.css is linked properly.</p>
+      <div className="wrapper" style={logoStyle}>
+        <img src={logo} alt="Logo" style={imgStyle} />;
       </div>
     )
   }
 }
-const pStyle = {
-    textAlign: "center",
+
+const logoStyle = {
+   textAlign: "center",
 }
+
+const imgStyle = {
+  width: "300px",
+}
+
 export default Header;
