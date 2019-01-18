d11cc1573ebfec13b8df928a3a65ed0b293795ea
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2019-01-18 13:13:01 -0600

    changed className

--- client/src/pages/play.js
+++ client/src/pages/play.js
@@ -7,27 +7,24 @@ import AnimalCard from "../components/AnimalCard";
 class Play extends Component {
   render() {
     return (
-     <div className="Site">
-      <div className="Site-content">
+      <div className="Site">
+        <div className="Site-content">
+          {/* Header */}
+          <div className="App-header">
+            <Header />
+          </div>
 
-            {/* Header */}
-        <div classname="App-header">
-          <Header />
-        </div>
-
-            {/* Main Game Card contents goes below: */}
-        <div className="main">
+          {/* Main Game Card contents goes below: */}
+          <div className="main">
             <AnimalCard />
-        </div>
-          
-            {/* Footer */}
-            <Footer />
-        </div>
+          </div>
 
-     </div>
-    )
+          {/* Footer */}
+          <Footer />
+        </div>
+      </div>
+    );
   }
 }
-
 
 export default Play;
