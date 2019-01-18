36fb0d04d1904e222989f14c851b8205ac605341
Zac Seewald <zaxblackdragon.com>
2019-01-18 08:33:01 -0600

    added demo card to login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import AnimalCard from "../components/animals";
+import AnimalImage from "../images/1920b077029194de4d8b40d585e4ddf6.png";
 
 class LogIn extends Component {
   render() {
@@ -15,9 +15,14 @@ class LogIn extends Component {
           <Header />
         </div>
 
-        {/* Main Game Card content goes below: */}
+        {/* Main Game Card contents goes below: */}
         <div className="main">
-          
+          <div className="card">
+            <div style={cardStyle}>
+              <img src={AnimalImage} alt="Logo" style={imageStyle} />
+              <p>Demo Card</p>
+            </div>
+          </div>
         </div>
           
         {/* Footer */}
@@ -27,6 +32,18 @@ class LogIn extends Component {
      </div>
     )
   }
+}
+const imageStyle={
+  width: "175px",
+  background: "#50D737",
+}
+const cardStyle = {
+  background: "white",
+  width: "175px",
+  border: "solid 5px purple",
+  borderRadius: "10px",
+  textAlign: "center",
+  marginLeft: "20px",
 }
 
 export default LogIn;
